df2d0c689af4a128293e60e11b9523da807c1334
SomyaBhatnagar19 <somya.bhatnagar98@gmail.com>
2023-12-18 11:11:57 +0530

    Installing nodemon, expressjs, sequelize, making servere.js and package.json files.

---
+++ backend/server.js
@@ -0,0 +1,11 @@
+/* backend/servere.js */
+
+const express = require('express');
+
+const app = express();
+
+app.use(express.json());
+
+app.listen(3000, () => {
+    console.log("Server is listening.... & runnning at port 3000.")
+})
